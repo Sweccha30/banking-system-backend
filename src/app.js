b76ff76f0f0c1bf3f30c8d84e3b7a4143e49e5f6
Sweccha <Escape>
2026-07-20 01:26:48 +0530

    Add user authentication and email notifications

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express")
-
-
+const cookieParser= require("cookie-parser")
+const authRouter = require("./routes/auth.routes")
 
 
 const app = express()
+
+
+app.use(express.json())            //middleware
+app.use(cookieParser())
+
+app.use("/api/auth", authRouter)
 
 
 module.exports = app
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,54 +1,52 @@
-const mongoose = require("mongoose")
-const bcrypt = require("bcryptjs")
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating a user"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/,
+        "Please enter a valid email.",
+      ],
+      unique: true,
+    },
 
-  email: {
-type: String,
-required: [ true , "Email is required for creating a user"],
-trim: true,
-lowercase: true,
-match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/,],
-unique: [true, "Email already exists." ]
+    name: {
+      type: String,
+      required: [true, "Name is required for creating an account"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required for creating an account"],
+      minlength: [6, "Password should contain at least 6 characters"],
+      select: false,
+    },
   },
+  {
+    timestamps: true,
+  }
+);
 
- name: {
-  tyep: String,
-  required:[true, "Name is required for creating an account" ]
- },
-
- password: {
-  type: String,
-  required: [true, "Password is required for creating an account "],
-  minlength:[6, "Password should contain more than 6 character"],
-  select: false,
- }
-},
- {
- timestamps: true
- })
-
-
- userSchema.pre("save", async function(next){
-
-if( !this.isModified("password")) {
-  return next()
-}
-
-const hash = await bcrypt.hash(this.password, 10)
-this.password = hash
- return next()
- })
-
-  userSchema.methods.comparePassword = async function(password){
-    return await bcrypt.compare(password, this.password)
-
+// Hash password before saving
+userSchema.pre("save", async function () {
+   console.log("🔥 pre-save middleware is running");
+  if (!this.isModified("password")) {
+    return;
   }
 
-  const userModel = mongoose.model("user", userSchema)
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
-  module.exports = userModel
+// Compare entered password with hashed password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
+const userModel = mongoose.model("user", userSchema);
 
-
-
+module.exports = userModel;
